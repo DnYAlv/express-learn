1714c7a0c69d0b23eb9c311c72266751f533141c
DnYAlv <dennyginting40@gmail.com>
2023-11-12 22:54:27 +0700

    Completed REST API for Products

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,7 +1,13 @@
 import { type Request, type Response } from 'express'
-import { createProductValidation } from '../validations/product.validation'
+import { createProductValidation, updateProductValidation } from '../validations/product.validation'
 import { logger } from '../utils/logger'
-import { addProductToDB, getProductById, getProductFromDB } from '../services/product.service'
+import {
+  addProductToDB,
+  deleteProductById,
+  getProductById,
+  getProductFromDB,
+  updateProductById
+} from '../services/product.service'
 import { v4 as uuidv4 } from 'uuid'
 // import type ProductType from '../types/product.type'
 
@@ -40,6 +46,51 @@ export const createProduct = async (req: Request, res: Response) => {
     logger.info('Success added new product')
   } catch (error) {
     logger.error('ERR: product - create = ', error)
+    return res.status(422).send({ status: false, statusCode: 422, message: error })
+  }
+}
+
+export const updateProduct = async (req: Request, res: Response) => {
+  const {
+    params: { id }
+  } = req
+
+  const { error, value } = updateProductValidation(req.body)
+  if (error) {
+    logger.error('ERR: Product - Update = ', error.details[0].message)
+    return res.status(422).send({ status: false, statusCode: 422, message: error.details[0].message, data: {} })
+  }
+  try {
+    const result = await updateProductById(id, value)
+    if (result) {
+      logger.info('Success Updated Product')
+      res.status(200).send({ status: true, statusCode: 200, message: 'Update Product Success' })
+    } else {
+      logger.info('Product Not Found')
+      res.status(404).send({ status: false, statusCode: 404, message: 'Product Not Found' })
+    }
+  } catch (error) {
+    logger.error('ERR: Product - Update', error)
+    return res.status(422).send({ status: false, statusCode: 422, message: error })
+  }
+}
+
+export const deleteProduct = async (req: Request, res: Response) => {
+  const {
+    params: { id }
+  } = req
+
+  try {
+    const result = await deleteProductById(id)
+    if (result) {
+      logger.info('Product Deleted Successfully')
+      res.status(200).send({ status: true, statusCode: 200, message: 'Product Deleted Successfully' })
+    } else {
+      logger.info('Product Not Found')
+      res.status(404).send({ status: false, statusCode: 404, message: 'Product Not Found' })
+    }
+  } catch (error) {
+    logger.error('ERR: Product - Delete = ', error)
     return res.status(422).send({ status: false, statusCode: 422, message: error })
   }
 }
